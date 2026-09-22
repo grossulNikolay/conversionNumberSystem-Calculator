7dd8a0e23b3be184847fc806962a8d4602160482
ISEEDEADPEOPLE <nickolas.ross@icloud.com>
2026-09-22 17:38:39 +0300

    Implement isValidC method for systemC validation

---
+++ src/IsValidC.java
@@ -0,0 +1,37 @@
+public class IsValidC {
+    public static boolean isValidC(String systemC) { //валидность только true/false
+
+        if (systemC.equalsIgnoreCase("Факт") || systemC.equalsIgnoreCase("Фак")) { //Факт СС
+            return true;
+        }
+
+        if (systemC.equalsIgnoreCase("Фиб")) { //Фиб СС
+            return true;
+        }
+
+        String letterPartB = systemC.toUpperCase(); //Симм СС
+        // ДОПИСАТЬ (СМ. VALIDITY CHECK)
+        if (letterPartB.endsWith("C") || letterPartB.endsWith("С")) {
+            String digitPartB = letterPartB.substring(0, letterPartB.length() -1);
+            try {
+                int radix = Integer.parseInt(digitPartB);
+                if (radix >= 3 && radix % 2 == 1) {
+                    return true;
+                }
+            } catch (Exception error) {
+                return false;
+            }
+        }
+
+        try { //числовые СС (2-36)
+            int radix = Integer.parseInt(systemC);
+            if (Math.abs(radix) < 2 || Math.abs(radix) > 36) { //+нега-позиционные
+                return false;
+            }
+            return true;
+
+        } catch (Exception error) {
+            return false;
+        }
+    }
+}
