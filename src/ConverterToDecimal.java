d5d501117745b4d686d003a9e27cd1f7bc65954a
ISEEDEADPEOPLE <nickolas.ross@icloud.com>
2026-09-24 01:34:21 +0300

    Change decimal conversion to use long type

--- src/ConverterToDecimal.java
+++ src/ConverterToDecimal.java
@@ -3,13 +3,13 @@ public class ConverterToDecimal {
             2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269,
             2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141,
             267914296, 433494437, 701408733, 1134903170, 1836311903};
-    public static final int[] factNumbers = new int[]{1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800,
+    public static final int[] factNumbers = new int[]{1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800,
             39916800, 479001600}; //задали 2 неизменяемых массива для Факт и Фиб СС
 
-    public static int convertAtoDecimal(String numberA, String systemB) { //10-ричные можно в int
+    public static long convertAtoDecimal(String numberA, String systemB) { //10-ричные можно в int
 
         if (systemB.equalsIgnoreCase("Фиб")) { //Фиб СС
-            int decimalNumber = 0;
+            long decimalNumber = 0;
             int length = numberA.length();
             for (int index = length - 1; index >= 0; index--) {
                 char ch = numberA.charAt(index);
@@ -22,7 +22,7 @@ public class ConverterToDecimal {
         }
 
         if (systemB.equalsIgnoreCase("Факт") || systemB.equalsIgnoreCase("Фак")) { //Факт СС
-            int decimalNumber = 0;
+            long decimalNumber = 0;
             int length = numberA.length();
             for (int index = length - 1; index >= 0; index--) {
                 char ch = numberA.charAt(index);
@@ -34,18 +34,17 @@ public class ConverterToDecimal {
         }
 
         String literalPartB = systemB.toUpperCase(); //Симм СС
-        //ДОПИСАТЬ (СМ. VALIDITY CHECK)
         if (literalPartB.endsWith("C") || literalPartB.endsWith("С")) {
             String digitPartB = literalPartB.substring(0, literalPartB.length() -1);
             int radix = Integer.parseInt(digitPartB);
-            int decimalNumber = 0;
-            int power = 1;
+            long decimalNumber = 0;
+            long power = 1;
             int halfRadix = radix / 2;
-            int lenght = numberA.length();
+            int length = numberA.length();
 
-            for (int i = lenght - 1; i >=0; i--) {
+            for (int i = length - 1; i >=0; i--) {
                 char ch = numberA.charAt(i);
-                int digit = Character.digit(ch, 10);
+                int digit = Character.digit(ch, radix); //radix != 10, долго искал баг после копипаста
 
                 if (digit > halfRadix) {
                     digit -= radix;
@@ -57,13 +56,13 @@ public class ConverterToDecimal {
             return decimalNumber;
         }
 
-        try { //числовые СС (2-36) +нега-позиционные
+        try { //числовые СС (2-36) +нега-позиционные(поэтому try даже после валидации)
             int radix = Integer.parseInt(systemB);
 
             if (radix < 0) {
-                int decimalNumber = 0;
+                long decimalNumber = 0;
                 int length = numberA.length();
-                int power = 1;
+                long power = 1;
                 boolean isNegative = false;
 
                 for (int i = length - 1; i >= 0; i--) {
@@ -87,11 +86,11 @@ public class ConverterToDecimal {
                 }
             }
 
-        int decimalNumber = Integer.parseInt(numberA, radix); //числовые СС (2-36)
+        long decimalNumber = Long.parseLong(numberA, radix); //числовые СС (2-36) !!!Long.parseLong()
         return decimalNumber;
 
         } catch (Exception error) {
-            return 0; //костыль, как в других методах (без этой строчки) не получается -- выдает ошибку
+            return 0;
         }
     }
 }
