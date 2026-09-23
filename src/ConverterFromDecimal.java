d661749c72475f2d2c752169f01daca6bf6c4431
ISEEDEADPEOPLE <nickolas.ross@icloud.com>
2026-09-24 01:35:01 +0300

    Change decimalNumber type to long in convertFromDecimal
    Updated the convertFromDecimal method to accept long instead of int for decimalNumber. Enhanced handling for various numeral systems including Fibonacci and negative positional systems.

--- src/ConverterFromDecimal.java
+++ src/ConverterFromDecimal.java
@@ -4,23 +4,22 @@ public class ConverterFromDecimal {
             2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141,
             267914296, 433494437, 701408733, 1134903170, 1836311903};//задали неизменяемый массив для Фиб СС
 
-    public static String convertFromDecimal(int decimalNumber, String systemC) { //мб любой ответ,
+    public static String convertFromDecimal(long decimalNumber, String systemC) { //мб любой ответ,
         // поэтому работаем со строками
 
         if (systemC.equalsIgnoreCase("Фиб")) {
             if (decimalNumber < 0) { //для Фиб СС только неотрицательные
                 return "ОШИБКА!!!";
             }
-
             if (decimalNumber == 0) {
                 return "0";
             }
 
             int index = 0;
             while (index < fibNumbers.length && fibNumbers[index] <= decimalNumber) {
-                index++;
+                index++; //жадный метод, сначала найдем максимально возможный индекс
             }
-            index--;
+            index--; //имхо проще так после цикла вернуться на шаг назад
 
             String result = "";
 
@@ -40,7 +39,6 @@ public class ConverterFromDecimal {
             if (decimalNumber < 0) { //для Факт СС только неотрицательные
                 return "ОШИБКА!!!";
             }
-
             if (decimalNumber == 0) {
                 return "0";
             }
@@ -49,13 +47,71 @@ public class ConverterFromDecimal {
             int divider = 2;
 
             while (decimalNumber > 0) {
-                int remainder = decimalNumber % divider;
+                long remainder = decimalNumber % divider;
                 result = remainder + result;
                 decimalNumber /= divider;
                 divider++;
             }
             return result;
         }
-        return "."; //пока затычка для Симм СС, нега-позиционных и дефолтных
+
+        String literalPartC = systemC.toUpperCase(); //Симм СС
+        if (literalPartC.endsWith("C") || literalPartC.endsWith("С")) {
+            if (decimalNumber == 0) {
+                return "0";
+            }
+
+            String digitPartC = literalPartC.substring(0, literalPartC.length() - 1);
+            int radix = Integer.parseInt(digitPartC);
+            int halfOfRadix = radix / 2;
+            String result = "";
+
+            while (decimalNumber != 0) {
+                long remainder = decimalNumber % radix; //long decimalNumber
+                decimalNumber /= radix;
+
+                if (remainder > halfOfRadix) {
+                    remainder -= radix;
+                    decimalNumber++;
+                } else if (remainder < -halfOfRadix) {
+                    remainder += radix;
+                    decimalNumber--;
+                }
+
+                if (remainder < 0) {
+                    remainder += radix;
+                }
+
+                char ch = Character.forDigit((int) remainder, radix); //remainder был long
+                result = ch + result;
+            }
+            return result.toUpperCase();
+        }
+
+        int radix = Integer.parseInt(systemC); //числовые СС (2-36) +нега-позиционные
+
+        if (decimalNumber == 0) {
+            return "0";
+        }
+
+        if (radix < 0) { //нега-позиционные
+            String result = "";
+
+            while (decimalNumber != 0) {
+                long remainder = decimalNumber % radix;
+                decimalNumber /= radix;
+
+                if (remainder < 0) {
+                    remainder -= radix;
+                    decimalNumber++;
+                }
+                char ch = Character.forDigit((int) remainder, Math.abs(radix));
+                result = ch + result;
+            }
+            return result.toUpperCase();
+        }
+
+        String result = Long.toString(decimalNumber, radix).toUpperCase();
+        return result.toUpperCase();
     }
 }
